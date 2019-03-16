141716fd46c67c737dd6111f6f28894b5e6d9cab
Tracy Ngo <tngo1206@gmail.com>
2019-03-16 18:39:58 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -9,5 +9,52 @@ class Song
   def save
     self.class.all << self
   end
+  
+  def self.create
+    song = self.new
+    song.save
+    song
+  end
+  
+  def self.new_by_name(name)
+    song = self.new
+    song.name = name
+    song
+  end
+  
+  def self.create_by_name(name)
+    song = self.new
+    song.name = name
+    song.save
+    song
+  end  
+  
+  def self.find_by_name(name)
+    self.all.find {|song| song.name == name}
+    
+  end
+  
+  def self.find_or_create_by_name(song)
+    
+    self.find_by_name(song) || self.create_by_name(song)
+  
+  end
+  
+  def self.alphabetical
+    self.all.sort_by{|word| word.name}
+  end
+  
+  def self.new_from_filename
+    
+  end
+  
+  def self.create_from_filename(song)
+    song.chomp(".mp3").
+    
+  end
+  
+  def self.destroy_all
+    self.all.clear
+  end
 
 end

63fd3e30084745c51660a963d35ca0fb4447048e
Tracy Ngo <tngo1206@gmail.com>
2019-03-16 20:23:35 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -47,7 +47,7 @@ class Song
   def self.new_from_filename(filename)
     song = self.new
     song.artist_name = filename.split(" - ")[0]
-    song.name = filename.split(" - ")[1].chomp(".mp3")
+    song.name= filename.split(" - ")[1].chomp(".mp3")
     
   end
   
